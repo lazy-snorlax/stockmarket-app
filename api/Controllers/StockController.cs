6112ea031f705a01d987128ebe91d71b55648da2
Anthony <blacknebula236@gmail.com>
2024-11-12 21:53:01 +1030

    create query object for stock filtering + implementing query object in repositories

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.DTOs.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -23,11 +24,11 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll() {
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query) {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            var stocks = await _stockRepo.GetAllAsync();
-            var stockDto = stocks.Select(s => s.ToStockDto());
+            var stocks = await _stockRepo.GetAllAsync(query);
+            var stockDto = stocks.Select(s => s.ToStockDto()).ToList();
             return Ok(stocks);
         }
 
@@ -61,7 +62,7 @@ namespace api.Controllers
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> DeleteById([FromRoute] int id) {
             if (!ModelState.IsValid) return BadRequest(ModelState);
-            
+
             var stockModel = await _stockRepo.DeleteAsync(id);
             if (stockModel == null) { return NotFound(); }
             return NoContent();
