aad9aa8697dbdcf8288b01ce4bd2feaaa26a536e
Anthony <blacknebula236@gmail.com>
2024-11-25 23:02:16 +1030

    add login form + prepping for login form validation

--- react/src/views/Login.tsx
+++ react/src/views/Login.tsx
@@ -1,13 +1,58 @@
 import React from "react";
+import * as Yup from 'yup'
 
 type Props = {}
 
+type LoginFormsInputs = {
+    userName: string
+    password: string
+}
+
+const validation = Yup.object().shape({
+    userName: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+})
+
 const LoginPage = (props: Props) => {
+    // const { loginUser } = null //useAuth
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     formState: { errors },
+    // } = useForm<LoginFormsInputs>({ resolver: yupResolver(validation) })
+
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <h1>
-                Sign in to your account
-            </h1>
+            <div className="hero-content">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">
+                        Login to your account
+                    </h1>
+                    <p className="py-6"></p>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
