77a8c70fb3a692a43c4ed756c917eb4d18c29da7
Anthony <blacknebula236@gmail.com>
2024-11-25 22:10:15 +1030

    setup tailwind & root settings

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom';
+import './assets/main.css'
+import { router } from './router/Routes.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
