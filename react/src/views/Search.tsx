4668a2ef786e8f3b4cf4cee504872050534659b9
Anthony <blacknebula236@gmail.com>
2025-03-25 23:29:47 +1030

    Setting up search component & page for findata api

--- react/src/views/Search.tsx
+++ react/src/views/Search.tsx
@@ -1,11 +1,39 @@
-
+import { useState } from "react"
+import CardList from "../components/CardList"
+import Search from "../components/Search"
 
 const SearchPage = () => {
+    const [search, setSearch] = useState<string>("")
+    const [searchResult, setSearchResult] = useState([])
+    const [serverError, setServerError] = useState<string | null>(null)
+
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    };
+
+    const onPortfolioCreate = (e: any) => {
+        e.preventDefault();
+        console.log(">>> creating portfolio")
+    };
+
+    const onSearchSubmit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        console.log(">>> searching for: ", search)
+        setSearchResult([])
+    };
+
     return (
         <>
-            <h1>This is a Search element</h1>
-            <h2>This is a list element</h2>
-            <h1>This is a cardlist element</h1>
+            <Search 
+                onSearchSubmit={onSearchSubmit} 
+                search={search}
+                handleSearchChange={handleSearchChange}
+            />
+            <CardList 
+                searchResults={searchResult} 
+                onPortfolioCreate={onPortfolioCreate} 
+            />
+            {serverError && <div>Unable to connect to API</div>}
         </>
     )
 }
