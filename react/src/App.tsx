505fa07c8cd308902143fabe22cca77c538cb127
Anthony <blacknebula236@gmail.com>
2025-03-28 21:06:15 +1030

    add ToastContainer

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { UserProvider } from './context/useAuth'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <UserProvider>
         <Navbar />
         <Outlet />
+        <ToastContainer />
       </UserProvider>
     </>
   )
