0f0835031af0d66a17d499ecfb48c57e8d13a621
Anthony <blacknebula236@gmail.com>
2025-03-28 21:17:58 +1030

    tweaking search page css

--- react/src/views/Search.tsx
+++ react/src/views/Search.tsx
@@ -3,9 +3,12 @@ import CardList from "../components/CardList"
 import Search from "../components/Search"
 import { CompanySearch } from "../company"
 import { searchCompanies } from "../api"
+import { PortfolioGet } from "../models/Portfolio"
+import { portfolioAddAPI } from "../services/PortfolioService"
 
 const SearchPage = () => {
     const [search, setSearch] = useState<string>("")
+    const [portfolioValues, setPortfolioValues] = useState<PortfolioGet[] | null>([])
     const [searchResult, setSearchResult] = useState<CompanySearch[]>([])
     const [serverError, setServerError] = useState<string | null>(null)
 
@@ -13,9 +16,17 @@ const SearchPage = () => {
         setSearch(e.target.value);
     };
 
-    const onPortfolioCreate = (e: SyntheticEvent) => {
+    const onPortfolioCreate = (e: any) => {
         e.preventDefault();
         console.log(">>> creating portfolio", e)
+        portfolioAddAPI(e.target[0].value)
+            .then((res) => {
+                if (res?.data) {
+                    setPortfolioValues(res?.data)
+                }
+            }).catch((e) => {
+                setPortfolioValues(null)
+            });
     };
 
     const onSearchSubmit = async (e: SyntheticEvent) => {
@@ -30,18 +41,26 @@ const SearchPage = () => {
     };
 
     return (
-        <>
-            <Search 
-                onSearchSubmit={onSearchSubmit} 
-                search={search}
-                handleSearchChange={handleSearchChange}
-            />
-            <CardList 
-                searchResults={searchResult} 
-                onPortfolioCreate={onPortfolioCreate} 
-            />
-            {serverError && <div>Unable to connect to API</div>}
-        </>
+        <div className=" bg-base-200 min-h-screen">
+            <div className="text-center lg:text-left">
+                <Search 
+                    onSearchSubmit={onSearchSubmit} 
+                    search={search}
+                    handleSearchChange={handleSearchChange}
+                />
+            </div>
+            <div className="hero">
+                <div className="hero-content">
+                    <div className="">
+                        {serverError && <div>Unable to connect to API</div>}
+                        <CardList 
+                            searchResults={searchResult} 
+                            onPortfolioCreate={onPortfolioCreate} 
+                            />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
